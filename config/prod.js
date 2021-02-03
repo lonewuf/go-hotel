5ffa70d3b36c3b7d68ca7fdf8e9fa63ec46354b2
John Nico <john@slabco.co>
2021-02-03 11:49:27 -0800

    changed mongodb uri

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,11 @@
 module.exports = {
-  MONGO_URI: "mongodb://niconiconi:niconiconi1@ds133221.mlab.com:33221/gohotel",
-  SECRET: "my sample secret",
+  // MONGO_URI: "mongodb://niconiconi:niconiconi1@ds133221.mlab.com:33221/gohotel",
+  MONGO_URI:
+    'mongodb+srv://niconiconi:niconiconi@cluster0.qskbw.mongodb.net/go-hotel-prod?retryWrites=true&w=majority',
+  SECRET: 'my sample secret',
   CLOUDINARY: {
-    NAME: "dpgjysglx",
-    KEY: "629347238193978",
-    SECRET: "7qafthx36UmgS2WYG1C2-YdRQ1Q",
+    NAME: 'dpgjysglx',
+    KEY: '629347238193978',
+    SECRET: '7qafthx36UmgS2WYG1C2-YdRQ1Q',
   },
 };
-
